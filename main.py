0dd0cff4f6c059386fa95e7384043a548f7358b9
marcell <marcell@list.ru>
2023-11-24 23:37:59 +0300

    Вернул, как было

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def hello():
-    print('Приветствую, пользователь!')
-
-
 if __name__ == '__main__':
 
-    hello()
     with open('data.txt', 'rb') as file:
         data = file.read()
     print('            00 01 02 03 04 05 06 07 08 09 0a 0b 0c 0d 0e 0f')
@@ -13,5 +8,3 @@ if __name__ == '__main__':
         hx = ' '.join(f'{hex(el)[2:]:0>2}' for el in data[i:i + 16])
         s = ''.join(chr(el) if chr(el).isprintable() else '.' for el in data[i:i + 16])
         print(f'{addr}    {hx:<48}     {s}')
-
-#  убрал комментарий
